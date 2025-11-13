b14928d8c4354ea8e0343c2e9cc478e1a3079d85
EddieH27 <141260227+EddieH27@users.noreply.github.com>
2025-11-13 18:38:06 +0000

    inequalities

--- week 13  csp/ 1 — Comparison Operators in Python.py
+++ week 13  csp/ 1 — Comparison Operators in Python.py
@@ -22,13 +22,18 @@ print(a <= b)   # True
 
 
 #predict the output of the following comparisons:
-10 > 5
-7 == 2 * 3 + 1
-8 != 8
-4 <= 2 + 2
+10 > 5  # true
+7 == 2 * 3 + 1    #true
+8 != 8           #false
+4 <= 2 + 2       #true
 
 # Write 3 examples that result in True and 3 that result in False.
-
+24 != 20
+56 < 60
+40 < 41
+20>25
+30<20
+1000<100
 # Create a simple grade-checking condition:
 
 # practice problem :

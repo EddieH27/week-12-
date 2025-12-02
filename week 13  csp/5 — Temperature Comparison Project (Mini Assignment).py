90d7faa0ae4adc93207c9d2be5959e68e1c59b5a
EddieH27 <141260227+EddieH27@users.noreply.github.com>
2025-12-02 19:23:28 +0000

    practice

--- week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
+++ week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
@@ -5,6 +5,19 @@
 # Write a program that:
 
 # Asks the user for today’s temperature.
+todays_temp = int(input('What is the temperature outside?'))
+if [-30] < todays_temp < [0]:
+    print('Its Freezing!')
+elif [0] < todays_temp < [32]:
+    print('Its really cold')
+elif [32] < todays_temp < [50]:
+    print("Its mad Brick out")
+elif [50] < todays_temp < [70]:
+    print('Its chilly')
+elif [70] < todays_temp < [90]:
+    print('Its Nice Out')
+elif [90] < todays_temp < [120]:
+    print('Its Really Hot!')
 
 # Prints whether it’s cold, warm, or hot using comparison operators.
 
